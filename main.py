1afd280929044259a222b948818c108291613387
Banchev1985 <55958209+Banchev1985@users.noreply.github.com>
2019-10-06 10:39:44 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import random
-
-words = ['neural', 'random', 'python', 'hello', 'lesson']
-
-words_choices = random.sample(words, random.randint(1, 3))
-
-text = str(random.randint(1, 9)).join(words_choices)
-
-print(text)
